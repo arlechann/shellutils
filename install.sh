9165186349dbf19acddf61e79e6695564c267f4a
arlechann <dragnov3728@gmail.com>
2021-10-02 01:26:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
 mkdir -p $HOME/bin
-ls $SCRIPT_PATH/bin | xargs -I{} unlink $HOME/bin/{} 2> /dev/null
-ls $SCRIPT_PATH/bin | xargs -I{} ln -s $SCRIPT_PATH/bin/{} $HOME/bin
+ls -1 $SCRIPT_PATH/bin | xargs -I{} unlink $HOME/bin/{} 2> /dev/null
+ls -1 $SCRIPT_PATH/bin | xargs -I{} ln -s $SCRIPT_PATH/bin/{} $HOME/bin
