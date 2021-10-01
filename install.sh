66ef9e20ea58c6607dbcc9963d2c523a05544776
arlechann <dragnov3728@gmail.com>
2021-10-02 03:10:03 +0900

    Add docker cli plugins

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ SCRIPT_PATH=$(cd $(dirname $0); pwd)
 mkdir -p $HOME/bin
 ls -1 $SCRIPT_PATH/bin | xargs -I{} unlink $HOME/bin/{} 2> /dev/null
 ls -1 $SCRIPT_PATH/bin | xargs -I{} ln -s $SCRIPT_PATH/bin/{} $HOME/bin
+
+[ -d "$HOME/.docker" ] && ln -s $SCRIPT_PATH/docker $HOME/.docker/cli-plugins
--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,4 @@
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
 ls $SCRIPT_PATH/bin | xargs -I{} unlink $HOME/bin/{}
+unlink $HOME/.docker/cli-plugins
