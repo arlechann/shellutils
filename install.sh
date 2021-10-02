9f7b4a425ceeb26102ddf8dcfbd6da1e7726c551
arlechann <dragnov3728@gmail.com>
2021-10-02 18:02:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mkdir -p $HOME/bin
 ls -1 $SCRIPT_PATH/bin | xargs -I{} unlink $HOME/bin/{} 2> /dev/null
 ls -1 $SCRIPT_PATH/bin | xargs -I{} ln -s $SCRIPT_PATH/bin/{} $HOME/bin
 
-[ -d "$HOME/.docker" ] && ln -s $SCRIPT_PATH/docker $HOME/.docker/cli-plugins
+[ -d "$HOME/.docker/cli-plugins" ] || mkdir -p ~/.docker/cli-plugins && ln -s $SCRIPT_PATH/docker $HOME/.docker/cli-plugins
